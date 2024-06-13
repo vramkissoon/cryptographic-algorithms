59f3462d6ce4e8658ac79193a6c0acc5853b4a39
vijayramkissoon <vramkissoon@proton.me>
2024-06-13 12:19:58 -0500

    Update caesar-cipher.py

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,20 +1,28 @@
 #caesar-cipher.py
 #Author: Vijay Ramkissoon
 
-def caesarEncrypt(plaintext,key,alphabet):
+def caesarEncrypt(plaintext,key,alphabet, punctuation = {'A':'', 'B':'.', 'C': '?', 'D':'!', 'E':',', 'F':'\'','G':'\"', 'H':';', 'I':':'}):
     if key > len(alphabet):
         key = key % len(alphabet)
     
     index_list = []
 
     for letter in plaintext:
-        index_list.append(alphabet.index(letter))
+        if letter in alphabet:
+            index_list.append(alphabet.index(letter))
+        else:
+            for index,value in punctuation.items():
+                if value == letter:
+                    index_list.append(index)
+
     print(index_list)
     
     shifted_list=[]
 
     for position in index_list:
-        if (position - key) < 0:
+        if type(position) is str:
+            shifted_list.append(position)
+        elif (position - key) < 0:
             shifted_list.append(len(alphabet) + position - key)
         else:
             shifted_list.append(position - key)
@@ -24,12 +32,15 @@ def caesarEncrypt(plaintext,key,alphabet):
     ciphertext = ''
 
     for new_position in shifted_list:
+        if type(position) is str:
+            ciphertext = ciphertext + new_position
         ciphertext = ciphertext + alphabet[new_position]
 
     print(ciphertext)
 
 
 english_alphabet = ['a','b','c', 'd', 'e', 'f', 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+english_punctuation = {'a':'', 'b':'.', 'c': '?', 'd':'!', 'e':',', 'f':'\'','g':'\"', 'h':';', 'i':':'}
 
 #Test 1
 p1 = 'def'
@@ -42,18 +53,24 @@ k2 = 3
 caesarEncrypt(p2,k2,english_alphabet)
 
 #Test3
-#p3 = 'attack left flank at noon'
-#k3 = 532
-#caesarEncrypt(p3,k3,english_alphabet)
+p3 = 'attack left flank at noon'
+k3 = 532
+caesarEncrypt(p3,k3,english_alphabet)
 
 
-def caesarDecrypt(ciphertext,key,alphabet):
+def caesarDecrypt(ciphertext,key, alphabet, punctuation = {'1':'', '2':'.', '3': '?', '4':'!', '5':',', '6':'\'','7':'\"', '8':';', '9':':'}):
     if key < len(alphabet):
         key = key % len(alphabet)
     
     index_list = []
-    for letter in ciphertext: 
-        index_list.append(alphabet.index(letter))
+    for letter in ciphertext:
+        if letter in alphabet: 
+            index_list.append(alphabet.index(letter))
+        else:
+            for pair in punctuation.items():
+                index,value = pair
+                if letter == index:
+                    index_list.append(value)
     
     print(index_list)
 
